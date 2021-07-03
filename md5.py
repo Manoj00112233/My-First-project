a5c106d1ea1e1dce0938e263aa9af216a05610fa
Manoj00112233 <56402420+Manoj00112233@users.noreply.github.com>
2021-07-03 22:55:18 +0530

    Update md5.py

--- md5.py
+++ md5.py
@@ -3,7 +3,7 @@ import hashlib
 result = hashlib.md5(b"Hello MD5").hexdigest()
 print(result)
 
-result = hashlib.md5("Hello MD5".encode("utf-8")).hexdigest()
+result = hashlib.md5("Hello Manoj is here".encode("utf-8")).hexdigest()
 print(result)
 
 m = hashlib.md5(b"Hello MD5")
